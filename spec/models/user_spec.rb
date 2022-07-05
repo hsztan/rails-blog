affc538853eabb7fa9e815f10dd402f0cfee05af
Henry Nawrocki <hdev@sztanski.com>
2022-07-05 14:22:13 -0500

    Add unit tests for model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe User, type: :model do
     subject.name = nil
     expect(subject).to_not be_valid
   end
+
+  it 'posts_counter should be integer' do
+    subject.posts_counter = 'string'
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be greater than or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it ':recent_posts should return 3 recent posts' do
+    3.times { Post.create(user: subject, title: 'Post title', text: 'Post text') }
+    expect(subject.recent_posts.size).to eq(3)
+  end
 end
