a4508e0180ddce6a860f138fce1d8a57faf607ef
Henry Nawrocki <hdev@sztanski.com>
2022-07-05 10:02:08 -0500

    Add User and Posts methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def increment_user_posts_count
+    user.posts_counter += 1
+    user.save
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
