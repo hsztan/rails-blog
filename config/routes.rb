6b207275e4d0c6a3e15264c62c8df51ab45bffd5
Henry Nawrocki <hdev@sztanski.com>
2022-07-09 12:53:53 -0500

    Add post views and correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  get "/users/user:id/posts" => "posts#index", as: :user_posts
-  get "/users/user:id/posts/:id" => "posts#show", as: :user_post
+  get "/users/:user_id/posts" => "posts#index", as: :user_posts
+  get "/users/:user_id/posts/:id" => "posts#show", as: :user_post
   get "/users" => "users#index", as: :users
   get "/users/:id" => "users#show", as: :user
 end
