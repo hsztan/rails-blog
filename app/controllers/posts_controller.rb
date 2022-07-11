98e3c534f9d8b327f13f313882777f4e9a5552c3
Henry Nawrocki <hdev@sztanski.com>
2022-07-11 12:41:37 -0500

    Finish User Posts Show View with Pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
+  POSTS_PER_PAGE = 2
+
   def index
+    @page = params.fetch(:page, 0).to_i
+    @page = 0 if @page.negative? || @page > (Post.count / POSTS_PER_PAGE)
     @user = User.find(params[:user_id])
+    @posts = @user.posts.offset(@page * POSTS_PER_PAGE).limit(POSTS_PER_PAGE)
   end
 
   def show; end
