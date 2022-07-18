f7d00a206064af0bcc64aeaee94334870333ae7a
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-18 11:19:50 -0500

    number of post & redirection tested

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe 'User index', type: :system do
         visit '/users'
         expect(page).to have_css('img')
     end
+
+    it 'displays the number of posts of the users' do
+        user1 = User.create(name: 'henry', photo: '', bio:'teacher from Poland')
+        user2 = User.create(name: 'james', photo: '', bio:'teacher from Mexico')
+        post1 = Post.create(title: 'post1', text: 'text1', user: user1)
+        post2 = Post.create(title: 'post2', text: 'text2', user: user2)
+        visit '/users'
+        expect(page).to have_content('1')
+        expect(page).to have_content('2')
+    end
+
+    it 'redirects to the user profile page when clicking the username' do
+        user1 = User.create(name: 'henry', photo: '', bio:'teacher from Poland')
+        user2 = User.create(name: 'james', photo: '', bio:'teacher from Mexico')
+        post1 = Post.create(title: 'post1', text: 'text1', user: user1)
+        post2 = Post.create(title: 'post2', text: 'text2', user: user2)
+        visit '/users'
+        click_link 'henry'
+        expect(page).to have_current_path('/users/1')
+    end
 end
