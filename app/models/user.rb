2e3ac3c37a5333a667bea5d66e03bef3b6c851f4
Henry Nawrocki <hdev@sztanski.com>
2022-07-19 17:00:42 -0500

    Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  # show user confirmation url on terminal
   has_many :posts
   has_many :comments
   has_many :likes
