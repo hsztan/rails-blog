c9c7c86b408291c47253466348fd46372d961a87
Henry Nawrocki <hdev@sztanski.com>
2022-07-19 13:45:04 -0500

    Add sign_up, sign_in, log_out buttons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   # Gmail SMTP settings for ActionMailer.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'smtp.mailtrap.io' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      '2c336ab673e7ac',
